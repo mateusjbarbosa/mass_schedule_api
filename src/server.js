216df64e51020d031b79a6890e68d87ae9fbe0a4
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-07 17:51:33 -0300

    Root route created for test

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 
-let app = express()
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send({
+      'message': 'Welcome to Mass Schedule API!',
+      'docs': "http://example.com/api/docs"
+    })
+})
 
 // Server up
 app.listen(3000, () => {
   console.log('Server initialiazed on :3000')
 })
+
+module.exports = app
