eae7e138f37cf3845b6ad1d18c1394c14f8c568e
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-21 16:06:23 -0300

    User model sequelize configuration

--- src/components/Users/user.model.ts
+++ src/components/Users/user.model.ts
@@ -1,43 +1,76 @@
-export default class User {
-  id: number;
-  telephoneNumber: string;
-  fullName: string;
-  dateBirth: string;
-  isTithe: boolean;
-  street: string;
-  houseNumber: string;
-  neighborhood: string;
-  city: string;
-  generalRecord: string;
-  individualRecord: string;
-  role: string;
-  celebration_allowed_count: number;
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
-  constructor(
-    telephoneNumber: string,
-    fullName: string,
-    dateBirth: string,
-    isTithe: boolean,
-    street: string,
-    houseNumber: string,
-    neighborhood: string,
-    city: string,
-    generalRecord: string,
-    individualRecord: string,
-    role: string,
-    celebration_allowed_count: number,
-  ) {
-    this.telephoneNumber = telephoneNumber;
-    this.fullName = fullName;
-    this.dateBirth = dateBirth;
-    this.isTithe = isTithe;
-    this.street = street;
-    this.houseNumber = houseNumber;
-    this.neighborhood = neighborhood;
-    this.city = city;
-    this.generalRecord = generalRecord;
-    this.individualRecord = individualRecord;
-    this.role = role;
-    this.celebration_allowed_count = celebration_allowed_count;
-  }
+@Table
+export default class User extends Model<User> {
+  @Column({
+    type: DataType.STRING(11),
+    allowNull: false,
+  })
+  telephoneNumber: string;
+
+  @Column({
+    type: DataType.STRING(100),
+    allowNull: false,
+  })
+  fullName: string;
+
+  @Column({
+    type: DataType.DATEONLY,
+    allowNull: false,
+  })
+  dateBirth: string;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: false,
+  })
+  isTithe: boolean;
+
+  @Column({
+    type: DataType.STRING(100),
+    allowNull: false,
+  })
+  street: string;
+
+  @Column({
+    type: DataType.STRING(5),
+    allowNull: false,
+  })
+  houseNumber: string;
+
+  @Column({
+    type: DataType.STRING(50),
+    allowNull: false,
+  })
+  neighborhood: string;
+
+  @Column({
+    type: DataType.STRING(100),
+    allowNull: false,
+  })
+  city: string;
+
+  @Column({
+    type: DataType.STRING(11),
+    allowNull: false,
+  })
+  generalRecord: string;
+
+  @Column({
+    type: DataType.STRING(11),
+    allowNull: false,
+  })
+  individualRecord: string;
+
+  @Column({
+    type: DataType.STRING(10),
+    allowNull: false,
+  })
+  role: string;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+  })
+  celebration_allowed_count: number;
 }
