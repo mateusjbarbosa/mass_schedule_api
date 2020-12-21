fe78cee50d448b104f6f38b3df882e95d5624b94
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-21 15:02:05 -0300

    Add UsersService in providers App

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './components/Users/Users.controller';
+import UsersService from './components/Users/users.service';
 @Module({
   imports: [],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
