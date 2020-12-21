7baf52804c61e9d7b7a870f6fa2b942eb88bc70e
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-21 15:01:44 -0300

    Refactory UsersController

--- src/components/Users/users.controller.ts
+++ src/components/Users/users.controller.ts
@@ -8,30 +8,36 @@ import {
   Post,
 } from '@nestjs/common';
 
+import User from './user.model';
+
+import UsersService from './users.service';
+
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post()
-  createUser(@Body() user): string {
-    return `created user`;
+  createUser(@Body() user: User): User {
+    return this.usersService.createUser(user);
   }
 
   @Get(':id')
-  getUser(@Param() params): string {
-    return `user ${params.id}`;
+  readUser(@Param() { id }): string {
+    return this.usersService.readUser(id);
   }
 
   @Get()
-  getAllUsers(): string {
-    return 'all users';
+  readAllUsers(): string {
+    return this.usersService.readAllUsers();
   }
 
   @Patch(':id')
-  updateUser(@Body() user): string {
-    return `updated user`;
+  updateUser(@Body() user: User): User {
+    return this.usersService.updateUser(user);
   }
 
   @Delete(':id')
-  deleteUser(@Param() params): string {
-    return `deleted user`;
+  deleteUser(@Param() { id }): string {
+    return this.usersService.deleteUser(id);
   }
 }
