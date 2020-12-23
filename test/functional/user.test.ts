ca134ab1a8ef9384dff4cab20a833849c04a02e1
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-23 16:57:43 -0300

    Created user test for creating sucess, creating when item data missing and when item data type error

--- test/functional/user.test.ts
+++ test/functional/user.test.ts
@@ -1,4 +1,8 @@
+import { User } from '@src/models/user';
+
 describe('User functional tests', () => {
+  beforeAll(async () => User.deleteMany({}));
+
   describe('When creating a user', () => {
     it('should create a user with sucess', async () => {
       const newUser = {
@@ -20,6 +24,57 @@ describe('User functional tests', () => {
       const response = await global.testRequest.post('/users').send(newUser);
 
       expect(response.status).toBe(204);
+      expect(response.headers['content-location']).toContain('/users/');
+    });
+
+    it('should return 422 when a user item data is missing', async () => {
+      const newUser = {
+        telephoneNumber: '35991282121',
+        dateBirth: '1999-10-03',
+        isTithe: true,
+        street: 'Rua Benedito Pinto dos Santos',
+        houseNumber: '350',
+        neighborhood: 'Santa Barbara',
+        city: 'Conceição dos Ouros',
+        state: 'MG',
+        generalRecord: 'MG18108433',
+        individualRecord: '14334545645',
+        role: 'FAITH',
+        celebration_allowed_count: 3,
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error: 'User validation failed: fullName: Path `fullName` is required.',
+      });
+    });
+
+    it('should return 422 when there is a type error', async () => {
+      const newUser = {
+        telephoneNumber: '35991282121',
+        fullName: 'Mateus José Barbosa',
+        dateBirth: '1999-10-03',
+        isTithe: true,
+        street: 'Rua Benedito Pinto dos Santos',
+        houseNumber: '350',
+        neighborhood: 'Santa Barbara',
+        city: 'Conceição dos Ouros',
+        state: 'MG',
+        generalRecord: 'MG18108433',
+        individualRecord: '14334545645',
+        role: 'FAITH',
+        celebration_allowed_count: 'is_3',
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'User validation failed: celebration_allowed_count: Cast to Number failed for value "is_3" at path "celebration_allowed_count"',
+      });
     });
   });
 
@@ -44,8 +99,6 @@ describe('User functional tests', () => {
           individualRecord: '14334545645',
           role: 'FAITH',
           celebration_allowed_count: 3,
-          updatedAt: '2020-12-23T12:04:16.175Z',
-          createdAt: '2020-12-23T12:04:16.175Z',
         },
         {
           id: 2,
