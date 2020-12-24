a2d051f78e370b78ff85778b0e2c292d0dd7c440
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-23 22:42:07 -0300

    Created api index caller

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('src');
+import './util/module-alias';
+
+import { SetupServer } from '@src/server';
+
+import config from 'config';
+
+(async (): Promise<void> => {
+  const server = new SetupServer(config.get('App.port'));
+
+  await server.init();
+  server.start();
+})();
