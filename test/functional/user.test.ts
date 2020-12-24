0409a2c6059b328ad52a0aa15d045c41af35b046
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-24 00:09:50 -0300

    Added improvements in user tests and test case for unique key validation

--- test/functional/user.test.ts
+++ test/functional/user.test.ts
@@ -8,52 +8,70 @@ describe('User functional tests', () => {
   beforeAll(async () => User.deleteMany({}));
 
   describe('When creating a user', () => {
-    it('should create a user with sucess', async () => {
-      const newUser = userRegisterCorrectFixture;
+    it('should sucessfully create a user', async () => {
+      const userBody = userRegisterCorrectFixture;
 
-      const response = await global.testRequest.post('/users').send(newUser);
+      const { headers, body, status } = await global.testRequest
+        .post('/users')
+        .send(userBody);
 
-      expect(response.status).toBe(204);
-      expect(response.headers['content-location']).toContain('/users/');
+      expect(status).toBe(201);
+      expect(headers['content-location']).toContain('/users/');
+      expect(body).toEqual(expect.objectContaining(userBody));
     });
 
     it('should return 422 when a user item data is missing', async () => {
-      const newUser = userRegisterWithoutFixture;
+      const userBody = userRegisterWithoutFixture;
 
-      const response = await global.testRequest.post('/users').send(newUser);
+      const { body, status } = await global.testRequest
+        .post('/users')
+        .send(userBody);
 
-      expect(response.status).toBe(422);
-      expect(response.body).toEqual({
+      expect(status).toBe(422);
+      expect(body).toEqual({
+        code: 422,
         error: 'User validation failed: fullName: Path `fullName` is required.',
       });
     });
 
     it('should return 422 when there is a type error', async () => {
-      const newUser = userRegisterTypeErrorFixture;
+      const userBody = userRegisterTypeErrorFixture;
 
-      const response = await global.testRequest.post('/users').send(newUser);
+      const { body, status } = await global.testRequest
+        .post('/users')
+        .send(userBody);
 
-      expect(response.status).toBe(422);
-      expect(response.body).toEqual({
+      expect(status).toBe(422);
+      expect(body).toEqual({
+        code: 422,
         error:
           'User validation failed: celebration_allowed_count: Cast to Number failed for value "is a 3" at path "celebration_allowed_count"',
+      });
+    });
+
+    it('should return 409 when the general record already exists', async () => {
+      const userBody = userRegisterCorrectFixture;
+
+      const { body, status } = await global.testRequest
+        .post('/users')
+        .send(userBody);
+
+      expect(status).toBe(409);
+      expect(body).toEqual({
+        code: 409,
+        error: 'User validation failed: generalRecord: already exists in the database, individualRecord: already exists in the database',
       });
     });
   });
 
   describe('When reading a user', () => {
     it('should return all registered users', async () => {
-      const user = userRegisterCorrectFixture;
+      const userBody = userRegisterCorrectFixture;
 
       const { body, status } = await global.testRequest.get('/users');
 
-      const expectedUser = [{
-        id: body[0]['id'],
-        ...user
-      }];
-
       expect(status).toBe(200);
-      expect(body).toEqual(expectedUser);
+      expect(body).toEqual([expect.objectContaining(userBody)]);
     });
   });
 });
