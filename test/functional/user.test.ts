81942970374cfec2c70d8b9585b9d0fdb7c300a8
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-23 22:15:11 -0300

    Changed json to fixtures and improvements in user tests

--- test/functional/user.test.ts
+++ test/functional/user.test.ts
@@ -1,25 +1,15 @@
 import { User } from '@src/models/user';
 
+import userRegisterCorrectFixture from '@test/fixtures/user_register_correct_fixture';
+import userRegisterTypeErrorFixture from '@test/fixtures/user_register_error_type_fixture';
+import userRegisterWithoutFixture from '@test/fixtures/user_register_without_fullName_fixture';
+
 describe('User functional tests', () => {
   beforeAll(async () => User.deleteMany({}));
 
   describe('When creating a user', () => {
     it('should create a user with sucess', async () => {
-      const newUser = {
-        telephoneNumber: '35991282121',
-        fullName: 'Mateus José Barbosa',
-        dateBirth: '1999-10-03',
-        isTithe: true,
-        street: 'Rua Benedito Pinto dos Santos',
-        houseNumber: '350',
-        neighborhood: 'Santa Barbara',
-        city: 'Conceição dos Ouros',
-        state: 'MG',
-        generalRecord: 'MG18108433',
-        individualRecord: '14334545645',
-        role: 'FAITH',
-        celebration_allowed_count: 3,
-      };
+      const newUser = userRegisterCorrectFixture;
 
       const response = await global.testRequest.post('/users').send(newUser);
 
@@ -28,20 +18,7 @@ describe('User functional tests', () => {
     });
 
     it('should return 422 when a user item data is missing', async () => {
-      const newUser = {
-        telephoneNumber: '35991282121',
-        dateBirth: '1999-10-03',
-        isTithe: true,
-        street: 'Rua Benedito Pinto dos Santos',
-        houseNumber: '350',
-        neighborhood: 'Santa Barbara',
-        city: 'Conceição dos Ouros',
-        state: 'MG',
-        generalRecord: 'MG18108433',
-        individualRecord: '14334545645',
-        role: 'FAITH',
-        celebration_allowed_count: 3,
-      };
+      const newUser = userRegisterWithoutFixture;
 
       const response = await global.testRequest.post('/users').send(newUser);
 
@@ -52,71 +29,31 @@ describe('User functional tests', () => {
     });
 
     it('should return 422 when there is a type error', async () => {
-      const newUser = {
-        telephoneNumber: '35991282121',
-        fullName: 'Mateus José Barbosa',
-        dateBirth: '1999-10-03',
-        isTithe: true,
-        street: 'Rua Benedito Pinto dos Santos',
-        houseNumber: '350',
-        neighborhood: 'Santa Barbara',
-        city: 'Conceição dos Ouros',
-        state: 'MG',
-        generalRecord: 'MG18108433',
-        individualRecord: '14334545645',
-        role: 'FAITH',
-        celebration_allowed_count: 'is_3',
-      };
+      const newUser = userRegisterTypeErrorFixture;
 
       const response = await global.testRequest.post('/users').send(newUser);
 
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
         error:
-          'User validation failed: celebration_allowed_count: Cast to Number failed for value "is_3" at path "celebration_allowed_count"',
+          'User validation failed: celebration_allowed_count: Cast to Number failed for value "is a 3" at path "celebration_allowed_count"',
       });
     });
   });
 
   describe('When reading a user', () => {
     it('should return all registered users', async () => {
+      const user = userRegisterCorrectFixture;
+
       const { body, status } = await global.testRequest.get('/users');
 
+      const expectedUser = [{
+        id: body[0]['id'],
+        ...user
+      }];
+
       expect(status).toBe(200);
-      expect(body).toEqual([
-        {
-          id: 1,
-          telephoneNumber: '35991282121',
-          fullName: 'Mateus José Barbosa',
-          dateBirth: '1999-10-03',
-          isTithe: true,
-          street: 'Rua Benedito Pinto dos Santos',
-          houseNumber: '350',
-          neighborhood: 'Santa Barbara',
-          city: 'Conceição dos Ouros',
-          state: 'MG',
-          generalRecord: 'MG18108433',
-          individualRecord: '14334545645',
-          role: 'FAITH',
-          celebration_allowed_count: 3,
-        },
-        {
-          id: 2,
-          telephoneNumber: '35991205542',
-          fullName: 'Alcino Teixeira',
-          dateBirth: '1978-15-12',
-          isTithe: false,
-          street: 'Rua das Flores',
-          houseNumber: '123',
-          neighborhood: 'Jardim América',
-          city: 'São Paulo',
-          state: 'SP',
-          generalRecord: '355215597',
-          individualRecord: '01609050878',
-          role: 'FAITH',
-          celebration_allowed_count: 3,
-        },
-      ]);
+      expect(body).toEqual(expectedUser);
     });
   });
 });
