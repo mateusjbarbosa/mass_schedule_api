12302c23c66d16f882a01a4e49d7b2eb2c99a2a0
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-29 16:56:04 -0300

    Ensure AcessRouter returns 400 if no date birth is provided

--- src/presentation/routes/acess.spec.js
+++ src/presentation/routes/acess.spec.js
@@ -1,6 +1,6 @@
 class AcessRouter {
   route (httpRequest) {
-    if (!httpRequest.body.phone_number) {
+    if (!httpRequest.body.phone_number || !httpRequest.body.date_birth) {
       return {
         statusCode: 400
       }
@@ -14,6 +14,18 @@ describe('Acess Router', () => {
     const httpRequest = {
       body: {
         date_birth: '01/01/1990'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no date birth is provided', () => {
+    const sut = new AcessRouter()
+    const httpRequest = {
+      body: {
+        phone_number: '99999999999'
       }
     }
     const httpResponse = sut.route(httpRequest)
