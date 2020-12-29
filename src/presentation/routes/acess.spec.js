f463ef64d6f795b18267e96084ef82a9556907cb
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-29 17:49:09 -0300

    Move SUT creation to a factory helper method

--- src/presentation/routes/acess.spec.js
+++ src/presentation/routes/acess.spec.js
@@ -1,9 +1,28 @@
 const AcessRouter = require('./acess')
 const RequiredParamError = require('../errors/required-param')
 
+const makeSut = () => {
+  return new AcessRouter()
+}
+
 describe('Acess Router', () => {
+  test('Should return 500 if no httpRequest is provided', () => {
+    const sut = makeSut()
+    const httpResponse = sut.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body', () => {
+    const sut = makeSut()
+    const httpRequest = {}
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
   test('Should return 400 if no phone number is provided', () => {
-    const sut = new AcessRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         dateBirth: '01/01/1990'
@@ -16,7 +35,7 @@ describe('Acess Router', () => {
   })
 
   test('Should return 400 if no date birth is provided', () => {
-    const sut = new AcessRouter()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         phoneNumber: '99999999999'
@@ -26,20 +45,5 @@ describe('Acess Router', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.message).toEqual(new RequiredParamError('dateBirth'))
-  })
-
-  test('Should return 500 if no httpRequest is provided', () => {
-    const sut = new AcessRouter()
-    const httpResponse = sut.route()
-
-    expect(httpResponse.statusCode).toBe(500)
-  })
-
-  test('Should return 500 if httpRequest has no body', () => {
-    const sut = new AcessRouter()
-    const httpRequest = {}
-    const httpResponse = sut.route(httpRequest)
-
-    expect(httpResponse.statusCode).toBe(500)
   })
 })
