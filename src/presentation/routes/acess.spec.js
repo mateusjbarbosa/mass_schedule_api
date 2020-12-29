8157c146cc5589cfc4b831b2330269f90103d11e
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2020-12-29 17:14:44 -0300

    Ensure AcessRouter returns 500 if an invalid httpRequest is provided

--- src/presentation/routes/acess.spec.js
+++ src/presentation/routes/acess.spec.js
@@ -1,6 +1,13 @@
 class AcessRouter {
   route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500
+      }
+    }
+
     const { phoneNumber, dateBirth } = httpRequest.body
+
     if (!phoneNumber || !dateBirth) {
       return {
         statusCode: 400
@@ -32,5 +39,20 @@ describe('Acess Router', () => {
     const httpResponse = sut.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return 500 if no httpRequest is provided', () => {
+    const sut = new AcessRouter()
+    const httpResponse = sut.route()
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if httpRequest has no body', () => {
+    const sut = new AcessRouter()
+    const httpRequest = {}
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
